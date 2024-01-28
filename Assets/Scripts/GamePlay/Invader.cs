c45491b8a6ad54395f659e8eb1bbc532fd97e000
LAPTOP-TV461787\Erek Xue <erekpic@gmail.com>
2024-01-28 00:56:50 -0500

    角色动画状态机做到一半

--- Assets/Scripts/GamePlay/Invader.cs
+++ Assets/Scripts/GamePlay/Invader.cs
@@ -8,6 +8,16 @@ public class Invader : MonoBehaviour
 
     public float vel = 3f;
 
+    //invaterAnimation用的
+    public Animator invaderAnim;
+    private float LeftRight;
+    private float ForwardBack;
+    private bool Jump;
+    private bool LftSwoosh;
+    private bool RhtSwoosh;
+
+    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -32,6 +42,59 @@ public class Invader : MonoBehaviour
         Vector3 moveVec = rawMoveVec.x * tanVec + rawMoveVec.y * radVec;
         transform.position += moveVec * vel * Time.deltaTime;
 
-
+        //33写的invader的状态机
+        LeftRight = Input.GetAxisRaw("Horizontal");
+        ForwardBack = Input.GetAxisRaw("Vertical");
+        CheckMoveCtrls();
     }
+
+    void FixedUpdate()
+    {
+        //check move controls and apply speed accordingly
+        //CheckMoveCtrls();
+
+        //update animator parameters
+        invaderAnim.SetFloat("ForwardBack", ForwardBack);
+        invaderAnim.SetFloat("LeftRight", LeftRight);
+        invaderAnim.SetBool("Jump", Jump);
+        invaderAnim.SetBool("LftSwoosh", LftSwoosh);
+        invaderAnim.SetBool("RhtSwoosh", RhtSwoosh);
+    }
+    
+
+    void CheckMoveCtrls()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump = true;
+            invaderAnim.SetTrigger("Jump 0");
+            Debug.Log("N");
+        }
+        else
+        {
+            Jump = false;
+        }
+
+        if ((Input.GetKey(KeyCode.A)) && (Input.GetKey(KeyCode.LeftShift)))
+        {
+            LftSwoosh = true;
+        }
+        else
+        {
+            LftSwoosh = false;
+        }
+
+        if ((Input.GetKey(KeyCode.D)) && (Input.GetKey(KeyCode.LeftShift)))
+        {
+            RhtSwoosh = true;
+        }
+        else
+        {
+            RhtSwoosh = false;
+        }
+    }
+
+    
+
+
 }
