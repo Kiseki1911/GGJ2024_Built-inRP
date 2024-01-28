5a4a9848e06abf72218b3674f486827dc52953d3
Shah Xia <sulummm@gmail.com>
2024-01-28 11:03:46 -0500

    修改 SceneChangeMar
    增加 Scene Change 逻辑

--- Assets/SceneChangeManager.cs
+++ Assets/SceneChangeManager.cs
@@ -5,11 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class SceneChangeManager : Singleton<SceneChangeManager>
 {
-    public bool switchScene = false;
+    private enum SceneState
+    {
+        StartScene,
+        GameSpeechScene,
+        ReplayVideoScene,
+        VictoryScene
+    }
+
+    private SceneState currentSceneState;
+    public bool switchScene; // Declare the switchScene variable
+
     // Start is called before the first frame update
     void Start()
     {
         DontDestroyOnLoad(this);
+        currentSceneState = SceneState.StartScene; // Set initial state
     }
 
     // Update is called once per frame
@@ -17,17 +28,34 @@ public class SceneChangeManager : Singleton<SceneChangeManager>
     {
         if (switchScene)
         {
-            if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("StartScene"))
-            {
-                SceneManager.LoadScene("GameplayTestScene",LoadSceneMode.Single);
-                //SceneManager.UnloadSceneAsync(0);
-            }
-            else{
-                SceneManager.LoadScene("StartScene",LoadSceneMode.Single);
-                //SceneManager.UnloadSceneAsync(1);
+            switchScene = false; // Reset the switch flag
+            ChangeScene();
+        }
+    }
 
-            }
-            switchScene = false;
+    private void ChangeScene()
+    {
+        switch (currentSceneState)
+        {
+            case SceneState.StartScene:
+                SceneManager.LoadScene("GameSpeechScene", LoadSceneMode.Single);
+                currentSceneState = SceneState.GameSpeechScene;
+                break;
+            case SceneState.GameSpeechScene:
+                SceneManager.LoadScene("ReplayVideoScene", LoadSceneMode.Single);
+                currentSceneState = SceneState.ReplayVideoScene;
+                break;
+            case SceneState.ReplayVideoScene:
+                SceneManager.LoadScene("ReplayVideoScene", LoadSceneMode.Single);
+                currentSceneState = SceneState.ReplayVideoScene;
+                break;
+            case SceneState.VictoryScene:
+                SceneManager.LoadScene("StartAnimation", LoadSceneMode.Single);
+                currentSceneState = SceneState.VictoryScene;
+                break;
+            default:
+                Debug.LogError("Unknown Scene State");
+                break;
         }
     }
 }
